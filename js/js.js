62cf0faa9166263decf07739730702ebafb6030f
samasayed3 <168020835+samasayed3@users.noreply.github.com>
2024-04-29 20:49:13 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -212,41 +212,23 @@ function hcol1(x){
         }
         //سما سيد عبداللطيف احمد
         function ypop(){
-    // let x= confirm("do you want to call them");
-    // if (x) {
-        // Add code here to initiate the call
-//         alert("sorry the number is not avilable");
-//     } else {
-//         alert("Call canceled.");
-//     }
-// }
-// function ypopfed() {
-//         show_ypopfed();
-// }
-// function  show_ypopfed(){
-//     document.getElementById("ypopup").classList.add("open");
-// }
-// function hide_ypopfed(){
-//     document.getElementById("ypopup").classList.remove("open");
-// }
-       function ypop(){
-    let x= confirm("do you want to call them");
-    if (x) {
-        // Add code here to initiate the call
-        alert("sorry the number is not avilable");
-    } else {
-        alert("Call canceled.");
-    }
-}
-function ypopfed() {
-        show_ypopfed();
-}
-function  show_ypopfed(){
-    document.getElementById("ypopup").classList.add("open");
-}
-function hide_ypopfed(){
-    document.getElementById("ypopup").classList.remove("open");
-}     
+     let x= confirm("do you want to call them");
+     if (x) {
+         Add code here to initiate the call
+         alert("sorry the number is not avilable");
+     } else {
+         alert("Call canceled.");
+     }
+ }
+ function ypopfed() {
+         show_ypopfed();
+ }
+ function  show_ypopfed(){
+     document.getElementById("ypopup").classList.add("open");
+ }
+ function hide_ypopfed(){
+     document.getElementById("ypopup").classList.remove("open");
+ }     
     }
 //------------------------------------------------------------------------------------------------
 //search box
